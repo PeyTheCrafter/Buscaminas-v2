abba010f932d934bc31f7c3d9a29ee7a74f1319f
Pablo <Pablo@DESKTOP-O5I954G>
2018-03-10 12:10:13 +0100

    Métodos útiles para los cálculos previos a la creación del tablero.

---
+++ src/utiles/UtilesTablero.java
@@ -0,0 +1,18 @@
+package utiles;
+
+public class UtilesTablero {
+	/*
+	 * Calcula el lado a partir del número de casillas.
+	 */
+	public int calcularLado(int casillas) {
+		return (int) Math.floor(Math.sqrt(casillas));
+	}
+	
+	/*
+	 * Calcula el número de casillas total en función de las minas y de la dificultad.
+	 */
+	public int calcularCasillas(int numeroMinas, int relacion) {
+		int casillas = relacion * numeroMinas;
+		return casillas;
+	}
+}
